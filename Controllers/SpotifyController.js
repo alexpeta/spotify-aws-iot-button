9291345579a950fc51ee570d7ca2b0ab5ff6d315
Alex Peta <alex.peta@macys.com>
2019-03-08 12:06:35 -0500

    changed exposed endpoints to post

--- Controllers/SpotifyController.js
+++ Controllers/SpotifyController.js
@@ -8,14 +8,14 @@ var spotifyTokenManager = require('../Managers/SpotifyTokenManager');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get('/nextTrack', function(request, response) {
+router.post('/nextTrack', function(request, response) {
     spotifyTokenManager.GetTokenAsync()
         .then(tokenModel => spotifyManager.NextTrackAsync(tokenModel))
         .then(response.status(200).send({message:'track change triggered'}))
         .catch(err => response.status(500).send({message:err.message, stack:err.stack}));
 });
 
-router.get('/previousTrack', function(request, response){
+router.post('/previousTrack', function(request, response){
     spotifyTokenManager.GetTokenAsync()
     .then(tokenModel => spotifyManager.PreviousTrackAsync(tokenModel))
     .then(response.status(200).send({message:'track change triggered'}))
