b37659db9d1f4f2972234ba77d3d0973da470881
Alex Peta <alex.peta@macys.com>
2019-03-10 15:05:29 -0400

    removed winston; moved more hard coded values to variables

--- Managers/AwsManager.js
+++ Managers/AwsManager.js
@@ -1,5 +1,6 @@
-var AWS = require("aws-sdk");
-var moment = require("moment");
+const AWS = require("aws-sdk");
+const moment = require("moment");
+const constants = require("../Utils/Constants");
 
 AWS.config.update({ region: "us-west-2" });
 
@@ -8,7 +9,7 @@ var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 function upsertTokenAsync(type, token, expiresIn) {
   return new Promise(function(resolve, reject) {
     var params = {
-      TableName: "tokens",
+      TableName: constants.TOKEN_TABLE_NAME,
       Item: {
         type: { S: type },
         token: { S: token },
@@ -35,7 +36,7 @@ function upsertTokenAsync(type, token, expiresIn) {
 
 function getAsync(type) {
   var params = {
-    TableName: "tokens",
+    TableName: constants.TOKEN_TABLE_NAME,
     Key: {
       type: { S: type }
     }
--- Utils/Constants.js
+++ Utils/Constants.js
@@ -1,4 +1,6 @@
 module.exports = Object.freeze({
   SPOTIFY_SCOPES: "user-modify-playback-state",
-  TOKEN_DELTA_SECONDS: 15
+  TOKEN_DELTA_SECONDS: 15,
+  TOKEN_KEY_ACCESS: "access",
+  TOKEN_TABLE_NAME: "tokens"
 });
